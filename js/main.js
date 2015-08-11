bc64489ba88e4f33fc2baa055839a15aacd05554
sergiocandrade <sergiocandrade@gmail.com>
2015-08-11 18:55:19 -0300

    Adicionando arquivos

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+    $( "select.ui-input" ).selectmenu();
+    $( "#datepicker" ).datepicker({
+    dateFormat: 'dd/mm/yy',
+    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+    nextText: 'Próximo',
+    prevText: 'Anterior',
+    closeText : 'Aplicar',
+    numberOfMonths: 2,
+    showButtonPanel:true,
+    beforeShow: function( input ) {  
+ setTimeout(function() {  
+   var buttonPane = $( input )  
+     .datepicker( "widget" )  
+     .find( ".ui-datepicker-buttonpane" );  
+  
+   var btn = $('<ul><li><button type="button" class="ui-datepicker-current ui-state-default ui-corner-all">Personalizado</button></li><li><button type="button" class="ui-datepicker-current ui-state-default ui-corner-all">Hoje</button></li><li><button type="button" class="ui-datepicker-current ui-state-default ui-corner-all">Ontem</button></li><li><button type="button" class="ui-datepicker-current ui-state-default ui-corner-all">Ultimos 7 dias</button></li><li><button type="button" class="ui-datepicker-current ui-state-default ui-corner-all">Ultimos 30 dias</button></li><li><button type="button" class="ui-datepicker-current ui-state-default ui-corner-all">Mês passado</button></li></ul>');  
+   btn  
+    .unbind("click")  
+   .bind("click", function () {  
+    $.datepicker._clearDate( input );  
+  });  
+  
+   btn.prependTo( buttonPane );  
+  
+ }, 1 );  
+      }  
+});
+});
+$(document).ready(function() {
+    $('.dropitmenu').dropit();
+});
