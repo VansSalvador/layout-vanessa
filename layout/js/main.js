9c9dcf4ba6aea4ec9c2fd85fceb2e84f3438b2c3
Fabricio Campos <fabriciob@gmail.com>
2015-08-24 00:22:18 -0300

    datepicker/validacao/modal criar/editar usuario/melhorias css

--- layout/js/main.js
+++ layout/js/main.js
@@ -2,53 +2,6 @@
 
 $(function() {
     $( "select.ui-input" ).selectmenu();
-    $( "#datepicker" ).datepicker({
-    dateFormat: 'dd/mm/yy',
-    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
-    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
-    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
-    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
-    nextText: 'Próximo',
-    prevText: 'Anterior',
-    closeText : 'Aplicar',
-    numberOfMonths: 2,
-    showButtonPanel:true,
-    beforeShow: function( input ) {
-        setTimeout(function() {
-            var buttonPane = $( input )
-                .datepicker( "widget" )
-                .find( ".ui-datepicker-buttonpane" );
-
-            $( "<button>", {
-                text: "Cancelar",
-                click: function() {
-                //Code to clear your date field (text box, read only field etc.) I had to remove the line below and add custom code here
-                    $.datepicker._clearDate( input );
-                }
-            }).appendTo( buttonPane ).addClass("ui-datepicker-clear ui-state-default ui-priority-primary ui-corner-all");
-            $( "<ul><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Personalizado</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Hoje</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Ontem</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Ultimos 7 dias</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Ultimos 30 dias</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Mês passado</button></li></ul>", {
-            }).prependTo( buttonPane ).addClass("ui-datepicker-clear ui-state-default ui-priority-primary ui-corner-all");
-        }, 1 );
-    },
-    onChangeMonthYear: function( year, month, instance ) {
-        setTimeout(function() {
-            var buttonPane = $( instance )
-                .datepicker( "widget" )
-                .find( ".ui-datepicker-buttonpane" );
-
-            $( "<button>", {
-                text: "Cancelar",
-                click: function() {
-                //Code to clear your date field (text box, read only field etc.) I had to remove the line below and add custom code here
-                    $.datepicker._clearDate( instance.input );
-                }
-            }).appendTo( buttonPane ).addClass("ui-datepicker-clear ui-state-default ui-priority-primary ui-corner-all");
-            $( "<ul><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Personalizado</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Hoje</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Ontem</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Ultimos 7 dias</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Ultimos 30 dias</button></li><li><button type='button' class='ui-datepicker-current ui-state-default ui-corner-all'>Mês passado</button></li></ul>", {
-            }).prependTo( buttonPane ).addClass("ui-datepicker-clear ui-state-default ui-priority-primary ui-corner-all");
-        }, 1 );
-    }
-});
 });
 $(document).ready(function() {
     $('.dropitmenu').dropit();
@@ -74,7 +27,14 @@ $(function() {
   $( ".deleteCat" ).click(function() {
      $( "#dialogDeleteCat" ).dialog( "open" );
   });
+  $( ".openCham" ).click(function() {
+     $( "#dialogOpenCham" ).dialog( "open" );
+  });
 });
 $(function() {
     $( ".tool" ).tooltip();
+});
+$(function() {
+  $('#datepick').datepick({ 
+    rangeSelect: true, monthsToShow: 2, showTrigger: '#calImg', renderer: $.datepick.weekOfYearRenderer, onShow: $.datepick.highlightWeek});
 });
