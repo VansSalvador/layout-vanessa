4dd33248758a7a304ce6982612c7717f195762f4
rodrigmars <rodrigmars@gmail.com>
2015-08-27 13:35:52 -0300

    Esqueleto do backend para login

--- painel.py
+++ painel.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,request,url_for
+from flaskext.auth import Auth,AuthUser,login_required
+import flask,time,hashlib
+
 app = Flask(__name__)
 
 jinja_options = app.jinja_options.copy()
@@ -11,6 +14,37 @@ jinja_options.update(dict(
     comment_end_string='#]',
 ))
 app.jinja_options = jinja_options
+app.secret_key='^mu0n!22#yqy=8a5x1hg5%1!2#dedsn=&cd&$ur3pzqiw+#$yd'
+
+auth=Auth(app)
+#auth.hash_algorithm='MD5'
+app.auth.hash_algorithm = lambda to_encrypt: hashlib.sha1(to_encrypt.encode('utf-8'))
+users={}
+
+@app.before_request
+def init_users():
+    admin = AuthUser(username='admin@epicom.com.br')
+    admin.set_and_encrypt_password('password')
+    admin.role='admin'
+    user = AuthUser(username='user@epicom.com.br')
+    user.set_and_encrypt_password('password')
+    user.role='user'
+    users['admin@epicom.com.br']= admin
+    users['user@epicom.com.br']=user
+
+@app.route('/login',methods=['POST'])
+def login():
+    username = request.json['user']
+    if username in users:
+        if users[username].authenticate(request.json['pass']):
+            return '302'
+    return '403'
+
+@app.route('/painel')
+@login_required()
+def logon():
+    return 'admin' if AuthUser.load_current_user().role=='admin' else 'user'
 
 if __name__ == '__main__':
+    app.SERVER_NAME='myapp.dev:5000'
     app.run(debug=True)
--- static/login.js
+++ static/login.js
@@ -1,6 +1,20 @@
 var app=angular.module('painel', []);
-app.controller('Login',['$scope',function($scope){
-    $scope.dologin=function(user,pass){
+app.controller('Login',['$scope','$http',function($scope,$http){
+    $scope.dologin=function(userp,passp){
+        $http.post('/login', {user:userp,pass:passp}).
+          then(function(response) {
+            if(response.data=='302'){
+                window.location.href='/painel';
+            }else{
+                alert("Falha de login!!");//TODO
+            }
+            // this callback will be called asynchronously
+            // when the response is available
+          }, function(response) {
+            alert("Erro!");//TODO
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
         console.log(user);
     };
 }]);
