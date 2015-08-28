2e8e3c9ad8e6dc7049fe04db298ce2ac683a4be5
Alex Henry <alex@epicom.com.br>
2015-08-28 13:41:34 -0300

    Clean up e refatoração

--- static/login.controller.js
+++ static/login.controller.js
@@ -1,15 +1,16 @@
 var app=angular.module('painel', []);
 app.controller('Login',['$scope','$http',function($scope,$http){
     $scope.dologin=function(userp,passp){
+        $scope.error='';
         $http.post('/login', {user:userp,pass:passp}).
           then(function(response) {
             if(response.data=='302'){
                 window.location.href='/painel';
             }else{
-                alert("Falha de login!!");//TODO
+                $scope.error='E-mail ou senha inválida.';
             }
           }, function(response) {
-            alert("Erro!");//TODO
+                $scope.error='Erro no sistema, tente mais tarde.';
           });
     };
 }]);
